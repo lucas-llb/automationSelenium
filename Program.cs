b35ccbda8c4b6a06462b8bd87360e047d707e964
Lucas Luciano Barbosa <lucas_llb@hotmail.com>
2024-09-07 16:39:34 -0300

    fix: missing correction

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using AutomationSelenium;
 
-var robot = new WhatsappMessagerRobot();
+var robot = new WhatsappMessegerRobot();
 
 // Put your message and the reciever here
 robot.SendMessage("Hello World", "Your-Group-Or-User-Here");
